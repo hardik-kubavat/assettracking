ccb0c9722504776768ca7b7ee5cf07a56f1d936c
Hardik Kubavat <kubavat.hardik@gmail.com>
2024-05-27 23:22:45 +0530

    fixed list files page

--- project/view/FileView.py
+++ project/view/FileView.py
@@ -15,7 +15,7 @@ def get_backup_status(file):
 
 @comv.route('/listfiles',methods=['GET','POST'])
 def addorupdate():
-    root_directory = "/home/bkp_server/backup/"
+    root_directory = os.getenv("BACKUP_DIR")
     files_list= []
     for subdir, dirs, files in os.walk(root_directory):
         if 'db' in subdir:
@@ -23,8 +23,7 @@ def addorupdate():
             latest_file = max(list_of_files, key=os.path.getctime)
             files_list.append(latest_file)
 
-    
     file_status = dict()
     for file in files_list:
         file_status[file.split('/')[-3].title()] = (datetime.fromtimestamp(os.path.getmtime(file)).strftime("%d-%m-%Y %H:%M"), file.split('/')[-3].title(), os.path.basename(file).split('/')[-1], get_backup_status(file))
-    return render_template("dashboard.html", backup_status=file_status)
+    return render_template("file_dashboard.html", backup_status=file_status, current_time = datetime.now().strftime("%d-%m-%Y %H:%M"))

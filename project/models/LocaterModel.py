79ba30c2a227e972fdf048e37e9822e00b5d8bfe
Hardik Kubavat <kubavat.hardik@gmail.com>
2024-06-06 23:13:39 +0530

    resolved minor issue

--- project/models/LocaterModel.py
+++ project/models/LocaterModel.py
@@ -41,7 +41,7 @@ class Locater(db.Model):
         self.description = description
     
 def getAll():
-    return Locater.query.all()
+    return Locater.query.order_by(Locater.name).all()
 
 def getById(locater_id):
     return Locater.query.filter_by(id=locater_id).first()
